5ab9b420dde1507949a332e4e52980a43794659e
fmpss1 <fmpss1@iscte-iul.pt>
2017-06-27 12:52:30 +0100

    002

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var ip = "localhost";	//127.0.0.1
+var port = process.env.PORT || 3000;
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+var routes = require("./routes/routes.js")(app);
+ 
+var server = app.listen(port, ip, function () {
+    console.log(`Servidor: Listening on http://${ip}:${port}`);
+});
---
+++ routes.js
@@ -0,0 +1,37 @@
+
+var appRouter = function(app) {
+
+//http://localhost:3000/
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+
+//http://localhost:3000/account?username=nraboy
+app.get("/account", function(req, res) {
+    var accountMock = {
+        "username": "nraboy",
+        "password": "1234",
+        "twitter": "@nraboy"
+    }
+    if(!req.query.username) {
+        return res.send({"status": "error", "message": "missing username"});
+    } else if(req.query.username != accountMock.username) {
+        return res.send({"status": "error", "message": "wrong username"});
+    } else {
+        return res.send(accountMock);
+    }
+});
+
+//http://localhost:3000/account?username=test?password=test?twitter=@test
+app.post("/account", function(req, res) {
+    if(!req.body.username || !req.body.password || !req.body.twitter) {
+        return res.send({"status": "error", "message": "missing a parameter"});
+    } else {
+        return res.send(req.body);
+    }
+});
+
+}
+ 
+module.exports = appRouter;
