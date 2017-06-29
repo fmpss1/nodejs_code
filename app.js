de55f291f9fc2075d57201d153d6610c85a55cc5
fmpss1 <fmpss1@iscte-iul.pt>
2017-06-29 18:34:43 +0100

    005 - Apresentação de dados na consola
    A questão dos childs está minimamente ultrapassada.
    Neste momento, estou a bater no array dentro do routes que pretendo ter acesso do app, para poder ter uma noção geral de todo o processo.

--- app.js
+++ app.js
@@ -9,9 +9,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var server = require("./server.js")(app);
 var routes = require("./routes/routes.js")(app);
+var array_childs_pids = require("./routes/routes.js").array_childs_pids;
+var array_childs_ports = require("./routes/routes.js").array_childs_ports;
+console.log(array_childs_pids);
 
-var os = require('os');
-var cpuCount = os.cpus().length;
-var cpuCount_obj = os.cpus().toString();
-console.log('Número de CPUs disponíveis: ' + cpuCount);
-console.log('Número de CPUs disponíveis: ' + cpuCount_obj);
+//var spawn = require('child_process').spawn;
+//var child = spawn('cmd', ['/c', 'dir']);
+
+var process_pid = process.pid;
+
+setInterval(function () {
+	var os = require('os');
+	var cpuCount = os.cpus().length;
+	console.log('\n\nNúmero de CPUs disponíveis: ' + cpuCount + '\nPlataforma: ' + process.platform);
+	console.log('\nProcesso PID do app: ' + process_pid + '\nNúmero de child PIDs: ' + array_childs_pids.length);
+	array_childs_pids.ForEach(function(i){
+		console.log(i);
+	});
+	
+	//Linux
+	//var spawn = require('child_process').spawn;
+	
+	
+	//console.log(child.pid);
+	
+	//Windows
+	//var spawn = require('child_process').spawn;
+	//var child = spawn('tasklist', ["/pid"]);
+	
+}, 10000);
--- routes.js
+++ routes.js
@@ -1,20 +1,27 @@
 "use strict";
 
+var array_childs_pids = ['1'];
+var array_childs_ports = ['1'];
+module.exports = array_childs_pids;
+module.exports = array_childs_ports;
+
+
 var appRouter = function(app) {
-//var SerdidorDaEquipa = require('./SerdidorDaEquipa.js');
+var SerdidorDaEquipa = "routes/SerdidorDaEquipa.js";
 var port = process.env.PORT || 4000;
 
-//localhost:3000/
+
+//http://localhost:3000/localhost:3000/
 app.get("/", function(req, res) {
-    res.send("API: <br><br> http://localhost:3000/account <br> http://localhost:3000/new_server <br> http://localhost:3000/stop_server");
+    res.send("API: <br><br> http://localhost:3000/account <br> http://localhost:3000/start_server <br> http://localhost:3000/stop_server");
 	console.log("\n Novo acesso -> " + Date());
 	//Nota: só quando se muda de browser é que o porto de origem muda.
 	console.log(' *** [Client] User remote address and port ' + req.connection.remoteAddress +':'+ req.connection.remotePort);
 	console.log(' *** [Server] User local  address and port ' + req.connection.localAddress +':'+ req.connection.localPort);
 });
 
-//localhost:3000/account?username=nraboy
-//localhost:3000/account?username=test
+//http://localhost:3000/localhost:3000/account?username=nraboy
+//http://localhost:3000/localhost:3000/account?username=test
 app.get("/account", function(req, res) {
     var accountMock = {
         "username": "nraboy",
@@ -37,23 +44,18 @@ app.get("/account", function(req, res) {
 	}
 });
 
-//localhost:3000/new_server
-app.get("/new_server", function(req, res) {	
-	//res.send("Teste novo servidor");
-	var cp = require('child_process');
-	var start_new_server_cmd = "node routes/SerdidorDaEquipa.js " + port + "";
-	//localhost:4000/ exemplo
-	cp.execSync('start cmd.exe /k "'+ start_new_server_cmd +'"');
+//http://localhost:3000/localhost:3000/start_server
+app.get("/start_server", function(req, res) {	
+	start_server(port);
 	port++;
 });
 
-app.get("/stop_server", function(req, res) {	
-	let object = new SerdidorDaEquipa ('Teste desliga servidor');
-	console.log(object);
-	res.send("Teste desliga servidor");
+//http://localhost:3000/localhost:3000/stop_server
+app.get("/stop_server", function(req, res) {
+	stop_server(port);
 });
 
-//localhost:3000/account?username=test?password=test?twitter=@test
+//http://localhost:3000/localhost:3000/account?username=test?password=test?twitter=@test
 app.post("/account", function(req, res) {
     if(!req.body.username || !req.body.password || !req.body.twitter) {
         return res.send({"status": "error", "message": "missing a parameter"});
@@ -64,3 +66,42 @@ app.post("/account", function(req, res) {
 
 }
 module.exports = appRouter;
+
+
+function start_server(port){
+	var spawn = require('child_process').spawn;
+	var child = spawn('node', ['routes/SerdidorDaEquipa.js', port]);
+	array_childs_pids.push(child.pid);
+	array_childs_ports.push(port);
+	console.log('\nProcesso PID ' + child.pid + ' do servidor no porto ' + port);
+	console.log(array_childs_pids);
+	console.log(array_childs_ports);
+	
+	child.stdin.write('Hello there!');
+	
+	child.stderr.on('data', function (data) {
+		console.log('There was an error: ' + data);
+	});
+	child.stdout.on('data', function (data) {
+		console.log('We received a reply: \n' + data);
+	});
+	child.stderr.on('data', function (data) {
+		console.log('There was an error: ' + data);
+	});
+}
+
+function stop_server(port){
+	
+	//Testar primeiro se existe servidores ativos
+	
+	setTimeout(function () {
+		console.log('Attempt to kill child process')
+		child.kill('SIGINT');
+		//child.kill('SIGUSR2'); //Parece que em windows não funciona
+	}, 5000);
+
+	// Actually kill the child process.
+	setTimeout(function () {
+		child.kill();
+	}, 10000);
+}
