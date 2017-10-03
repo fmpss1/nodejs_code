2f55b5299c314be23a562caf9b9cb570508de8de
fmpss1 <fmpss1@iscte-iul.pt>
2017-10-03 02:09:39 +0100

    v050 api.js file changes

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,54 @@ var client, dn, change;
 /** Global configurations */
 var config      = require('../config');
 
+var indexJSON = {"Access to the Simulator -> http://localhost:3000/api/" :
+                {
+                  "Login to access to the simulator" :
+                    "http://localhost:3000/api/login",
+                  "Create an account to access to the simulator" :
+                    "http://localhost:3000/api/account_create",
+                  "Documentation about this simulator API" :
+                    "http://localhost:3000/api/api_doc"}}
+
+var menuJSON = {"id" : "id",
+                    "team_name" : "team_name",
+                    "game_fase_number" : "number",
+                    "room_1"  : "iframe_url",
+                    "room_2"  : "iframe_url",
+                    "room_4"  : "iframe_url",
+                    "room_5"  : "iframe_url"}
+
+var errorMessageJSON = {"status": "error",
+                        "message": "Missing team|username|password|namespace"}
+
+
+var errorUserActivoJSON = {"status": "error",
+                           "message": "User already active"}
+
+var errorPasswordJSON = {"status": "error",
+                         "message": "Missing password"}
+
+
+
+
+
+
+//ACRESCENTAR AO SESSION E AO USERS.JSON:
+// ultimo acesso, quandos acessos, etc
+
+  //Nota: só quando se muda de browser é que o porto de origem muda.
+
+  //  config.logger.info("\n Novo acesso -> " + Date() +
+  //  '\n *** [Client] User remote address and port ' + 
+  //  req.connection.remoteAddress +':'+ req.connection.remotePort +
+  //  '\n *** [Server] User local  address and port ' +
+  //  req.connection.localAddress +':'+ req.connection.localPort);
+
+
+
+
+
+
 
 //var user         = 'cn=root';
 //var pass         = 'secret';
@@ -75,15 +123,7 @@ function reqHasPermission (req) {
 
 router.get('/', function (req, res, next) {
   if(req.headers['user-agent'].includes("curl"))
-    res.send({"Access to the Simulator -> http://localhost:3000/api/" :
-                {
-                  "Login to access to the simulator" :
-                    "http://localhost:3000/api/login",
-                  "Create an account to access to the simulator" :
-                    "http://localhost:3000/api/account_create",
-                  "Documentation about this simulator API" :
-                    "http://localhost:3000/api/api_doc"
-              }});
+    res.send(indexJSON);
 	else
     res.render('index');
 });
@@ -100,21 +140,21 @@ router.get('/api/users/:name', function (req, res) {
 });
 
 router.get('/user_secure', function (req, res, next) {
-  //res.render('user_secure', { flash: req.flash() } );
   res.render('user_secure', { name : req.session.dn } );
 });
 
 router.get('/admin_secure', function (req, res, next) {
-    res.render('admin_secure', { flash: req.flash() } );
+    res.render('admin_secure', { name : req.session.dn } );
 });
 
-router.get('/api_documentation', function (req, res, next) {
-	res.render('api_documentation', { flash: req.flash() } );
+router.get('/api_doc', function (req, res, next) {
+	res.render('api_doc');
 });
 
 router.get('/logout', function (req, res, next) {
   client = ldap.createClient({ url: config.URL_LDAP });
   dn = req.session.dn;
+  console.log(dn);
   if (req.query.token){
     modifyToken(dn, [""]);
     res.end();
@@ -130,10 +170,8 @@ router.get('/logout', function (req, res, next) {
 
 //http://localhost:3000/api/user?team=teste&username=teste&password=teste
 router.get('/login', function (req, res, next) {
-  if (!(req.query.team)){
-    //res.render('login', { flash: req.flash() } );
-  res.render('login');
-  }
+  if(!req.query.team)
+    res.render('login');
   else {
     client = ldap.createClient({ url: config.URL_LDAP });
     dn = 'cn='+req.query.username+', ou='+req.query.team+', o=ldap';
@@ -146,14 +184,7 @@ router.get('/login', function (req, res, next) {
         res.redirect('/api/admin_secure');
       }
       else{
-        var accessToken = Math.random();
-        console.log(accessToken);
-        res.setHeader ('accessToken', accessToken);
-        //return res.send(accessToken);
-        res.writeHead (200);
-        req.session.dn = 'cn='+req.query.username+' ,ou='+req.query.team+', o=ldap';
-        req.session.authenticated = true;
-        modifyToken(dn, accessToken);
+        req.session.dn = dn;
         modifyState(dn, ["active"]);
       }
       res.end();
@@ -162,164 +193,83 @@ router.get('/login', function (req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-  if(!(req.body.team && req.body.username && req.body.password)) {
-    return res.send({"status": "error", "message": "missing team|username|password"});
-  }
-  else if((req.body.team && req.body.username && req.body.password)) {
+  if(!(req.body.team && req.body.username && req.body.password))
+    res.send(errorMessageJSON);
+  else if(req.body.team && req.body.username && req.body.password) {
     client = ldap.createClient({ url: config.URL_LDAP });
     dn = 'cn='+req.body.username+', ou='+req.body.team+', o=ldap';
     client.bind(dn, req.body.password, function(err) {
-      if(err){
+      if(err)
         res.render('error_unauthorised', { status: 403 });
-      }
       else if(req.body.username=='admin' && req.body.password=='admin'){
-        req.session.authenticated = true;
+        req.session.dn = dn;
         res.redirect('/api/admin_secure');
+        modifyState(dn, ["active"]);
       }
       else{
         req.session.dn = dn;
-        modifyState(dn, ["active"]);
+        req.session.pw = req.body.password;
         res.redirect('/api/user_secure');
+        modifyState(dn, ["active"]);
       }
     });
   }
 });
 
-
-
-//http://localhost:3000/api/menu?team=teste&username=teste&password=teste&namespace=teste
+//curl "http://localhost:3000/api/menu?team=teste&username=teste&password=teste&namespace=teste" | jq
+//http://localhost:3000/api/menu
 router.get('/menu', function (req, res, next) {
-  if(!(req.query.team && req.query.username && req.query.password)) {
-    return res.send({"status": "error", "message": "missing team|username|password|namespace"});
-  }
-  else if((req.query.team && req.query.username && req.query.password)) {
-  client = ldap.createClient({ url: config.URL_LDAP });
-    dn = 'cn='+req.query.username+', ou='+req.query.team+', o=ldap';
-    client.bind(dn, req.query.password, function(err) {
-      if(err){
-        res.render('error_unauthorised', { status: 403 });
-      }
-      else if(req.query.username=='admin' && req.query.password=='admin'){
-        req.session.authenticated = true;
-        res.redirect('/api/admin_secure');
-      }
-      else{
-        var accessToken = Math.random();
-        console.log(accessToken);
-        res.setHeader ('accessToken', accessToken);
-        //return res.send(accessToken);
-        //res.writeHead (200);
-        req.session.dn = 'cn='+req.query.username+' ,ou='+req.query.team+', o=ldap';
-        req.session.authenticated = true;
-        //Tratar
-        //modifyToken(dn, accessToken);
-        //modifyState(dn, ["active"]);
-      }
-      res.send({"id" : "id",
-                    "team_name" : "team_name",
-                    "game_fase_number" : "number",
-                    "room_1"  : "iframe_url",
-                    "room_2"  : "iframe_url",
-                    "room_4"  : "iframe_url",
-                    "room_5"  : "iframe_url"
-                }
-      );
-    });
-  }
-});
-
-
-router.get('/account_create', function (req, res, next) {
-    console.log(req.session.dn);
-    res.render('account_create', { flash: req.flash() } );
-});
-
-router.get('/account_change', function (req, res, next) {
-    console.log(req.session.dn);
-	res.render('account_change', { flash: req.flash() } );
-});
-
-router.get('/account_change_pw', function (req, res, next) {
-    console.log(req.session.dn);
-  res.render('account_change_pw', { flash: req.flash() } );
-});
-
-router.get('/account_remove', function (req, res, next) {
-    res.render('account_remove', { flash: req.flash() } );
-});
-
-router.get('/account_search_user', function (req, res, next) {
-          console.log(req.session.dn);
-    res.render('account_search_user', { flash: req.flash() } );
-});
-
-router.get('/account_search_team', function (req, res, next) {
-    res.render('account_search_team', { flash: req.flash() } );
-});
-
-router.get('/admin_account_change_team', function (req, res, next) {
-    res.render('admin_account_change_team', { flash: req.flash() } );
-});
-
-router.get('/admin_account_create_team', function (req, res, next) {
-    res.render('admin_account_create_team', { flash: req.flash() } );
-});
-
-router.get('/admin_account_remove_team', function (req, res, next) {
-    res.render('admin_account_remove_team', { flash: req.flash() } );
-});
-
-router.get('/admin_account_search_team', function (req, res, next) {
-    res.render('admin_account_search_team', { flash: req.flash() } );
-});
-
-router.get('/admin_account_search_user', function (req, res, next) {
-    res.render('admin_account_search_user', { flash: req.flash() } );
-});
-
-
-
-
-
-
-
-
-router.post('/account_change_pw', function (req, res, next) {
-  if(!req.body.password) {
-    return res.send({"status": "error", "message": "missing password"});
-  }
+  if(req.session.dn)
+    res.send(menuJSON);
+  else if(!(req.query.team && req.query.username && req.query.password))
+    res.send(errorMessageJSON);
   else {
     client = ldap.createClient({ url: config.URL_LDAP });
-    modifyPassword(req);
-    if(req.body.username == 'admin'){
-      res.redirect('/api/admin_secure');
-    }
-    else{
-      res.redirect('/api/login');
-    }
+    dn = 'cn='+req.query.username+', ou='+req.query.team+', o=ldap';
+    client.bind(dn, req.query.password, function(err) {
+      if(err)
+        res.send(errorUserActivoJSON);
+      else
+        res.send(menuJSON);
+    });
   }
 });
 
 
 
 
-
-//ACRESCENTAR AO SESSION E AO USERS.JSON:
-// ultimo acesso, quandos acessos, etc
-
-  //Nota: só quando se muda de browser é que o porto de origem muda.
-
-  //  config.logger.info("\n Novo acesso -> " + Date() +
-  //  '\n *** [Client] User remote address and port ' + 
-  //  req.connection.remoteAddress +':'+ req.connection.remotePort +
-  //  '\n *** [Server] User local  address and port ' +
-  //  req.connection.localAddress +':'+ req.connection.localPort);
+router.get('/account_search_team', function (req, res, next) {
+    res.render('account_search_team');
+});
+router.get('/admin_remove_team', function (req, res, next) {
+    res.render('admin_remove_team');
+});
+router.get('/admin_remove_user', function (req, res, next) {
+    res.render('admin_remove_user');
+});
 
 
+router.get('/account_change_pw', function (req, res, next) {
+  res.render('account_change_pw');
+});
+router.post('/account_change_pw', function (req, res, next) {
+  if(!(req.body.password && req.body.new_password))
+    res.send(errorPasswordJSON);
+  else {
+    dn = req.session.dn;
+    client = ldap.createClient({ url: config.URL_LDAP });
+    modifyPassword(dn, req);
+    res.redirect('/api/login');
+    modifyState(dn, ["inactive"]);
+  }
+});
 
+router.get('/account_create', function (req, res, next) {
+    res.render('account_create');
+});
 router.post('/account_create', function (req, res, next) {
 	if(!(req.body.team && req.body.username && req.body.password)) {
-    return res.send({"status": "error", "message": "missing team|username|password"});
+    res.send(errorMessageJSON);
   }
   else {
     client = ldap.createClient({ url: config.URL_LDAP });
@@ -327,36 +277,22 @@ router.post('/account_create', function (req, res, next) {
   }
 });
 
-//Melhorar: mais opcoes
-router.post('/account_change', function (req, res, next) {
-	if(!(req.body.team && req.body.username && req.body.password &&
-		req.body.new_team && req.body.new_username && req.body.new_password)) {
-    return res.send({"status": "error", "message": "missing team|username|password"});
-  }
-  else {
-    client = ldap.createClient({ url: config.URL_LDAP });
-    modifyPassword(req);
-    if(req.body.username == 'admin'){
-      res.redirect('/api/admin_secure');
-    }
-    else{
-      res.redirect('/api/login');
-    }
-  }
+router.get('/account_remove', function (req, res, next) {
+    res.render('account_remove');
 });
-
 router.post('/account_remove', function (req, res, next) {
-  if(!(req.body.team && req.body.username && req.body.password)) {
-    return res.send({"status": "error", "message": "missing team|username|password"});
-  }
-  else {
-    deleteAccount(req);	
-  }
+  if(!(req.body.team && req.body.username && req.body.password))
+    res.send(errorMessageJSON);
+  else
+    deleteAccount(req);
 });
 
+router.get('/account_search_user', function (req, res, next) {
+    res.render('account_search_user');
+});
 router.post('/account_search_user', function (req, res, next) {
   if(!req.body.word) {
-    return res.send({"status": "error", "message": "missing username to search"});
+    res.send({"status": "error", "message": "missing username to search"});
   }
   else {
     var opts = {
@@ -406,10 +342,9 @@ router.post('/account_search_user', function (req, res, next) {
   }
 });
 
-function modifyPassword(req) {
-  dn = 'cn='+req.body.username+' ,ou='+req.body.team+', o=ldap';
+function modifyPassword(dn, req) {
   change = new ldap.Change({
-    operation: 'replace', modification: { userpassword: req.body.username.userPassword }
+    operation: 'replace', modification: { userpassword: req.body.new_password }
   });
   clientModify(dn, change);
 }
@@ -469,15 +404,12 @@ function createAccount(req, res){
 
 function deleteAccount(req){
   dn = 'cn='+req.body.username+' ,ou='+req.body.team+', o=ldap';
-  client.del(dn, function(err){
-    assert.ifError(err);
-    if(username == 'admin'){
-      res.render('admin_secure', { status: 403 });
-    }
-    else{
+  if(req.body.username != 'admin'){
+    client.del(dn, function(err){
+      assert.ifError(err);
       res.render('index', { status: 403 });
-    }
-  }); 
+    });
+  }
 }
 
 function unbind(){
